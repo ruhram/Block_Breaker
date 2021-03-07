1bec13fa8c1f6a4d3452a80cfd20efbebe1733d8
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-07 23:08:58 +0700

    paddle position coupling with mouse

---
+++ Assets/Scripts/paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition.x /Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
