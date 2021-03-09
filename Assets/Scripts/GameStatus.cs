9fa4f75cb5f1a10cd76f580806a8b68dd9ba988f
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-09 18:26:51 +0700

    Make Score UI

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameStatus : MonoBehaviour
 {
     [Range(.1f,10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int PointPerBlock = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int currentScore = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
@@ -22,5 +23,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore = currentScore + PointPerBlock;
+        scoreText.text = currentScore.ToString();
     }
 }
