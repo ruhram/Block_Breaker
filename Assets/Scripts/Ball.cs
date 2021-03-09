e573b9b09a02ad64d325729255600b893b3d7114
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-09 12:33:55 +0700

    Playing random audioClip

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,12 +8,15 @@ public class Ball : MonoBehaviour
     [SerializeField] paddle paddle1;
     [SerializeField] float Xpush = 2f;
     [SerializeField] float Ypush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
+    AudioSource myAudioSource;
     Vector2 paddleToBallVector;
     bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
+        myAudioSource = GetComponent<AudioSource>();
         paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
@@ -40,5 +43,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
