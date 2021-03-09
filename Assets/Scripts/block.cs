a0c47d7284aa8edd2fbea3c5d8ae16ed952b214c
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-09 14:37:54 +0700

    Adding level controller

--- Assets/Scripts/block.cs
+++ Assets/Scripts/block.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    level level;
 
+    private void Start()
+    {
+        level = FindObjectOfType<level>();
+        level.countBreakableBlock();
+
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
---
+++ Assets/Scripts/level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks;
+
+    public void countBreakableBlock()
+    {
+        breakableBlocks++;
+
+    }
+
+}
